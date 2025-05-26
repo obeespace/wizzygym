ba88b2b7ee6a14f0bde7d0c5954d42f94f8596ce
obeespace <94024823+obeespace@users.noreply.github.com>
2025-05-26 21:32:26 +0100

    fixed convert to number issues

--- app/components/PaymentForm.js
+++ app/components/PaymentForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const PaymentForm = () => {
-  const [email, setEmail] = useState("");
+  let email = localStorage.getItem("email")
   const [amount, setAmount] = useState("");
   const [paymentUrl, setPaymentUrl] = useState("");
   const [error, setError] = useState("");
@@ -27,21 +27,22 @@ const PaymentForm = () => {
       <h2 className="text-center mb-3">Make Payment</h2>
       <form onSubmit={handlePayment}>
         <input
-        className="text-black mb-2 rounded-sm px-2 py-1"
+        className="text-black mb-2 rounded-lg px-2 py-1"
           type="email"
           placeholder="Email"
+          disabled
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
           required
         />
-        <input
-          type="number"
-          className="text-black mb-2 rounded-sm px-2 py-1"
-          placeholder="Amount (NGN)"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          required
-        />
+        <select
+            value={amount} onChange={(e) => setAmount(e.target.value)}
+            className="border-b text-black border-red-600 px-3 py-1 w-full mb-5 rounded-md placeholder:text-gray-400 outline-none"
+          >
+            <option value="">Select a Plan</option>
+            <option value="2000">Day Plan - N2000</option>
+            <option value="15000">Monthly Plan - N15000</option>
+            <option value="40000">3 Months Plan - N40000</option>
+          </select>
         <button className="ml-3" type="submit">Pay Now</button>
       </form>
       {error && <p style={{ color: "red" }}>{error}</p>}
